7f083ef429af12384d63c5e98e919cac475ca579
Joe Teichman <kaalm1@gmail.com>
2017-05-18 00:02:44 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+def countdown(num)
+  (num).downto(1) do |number|
+    puts "#{number} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(num)
+  (num).downto(1) do |number|
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+  end
 end
